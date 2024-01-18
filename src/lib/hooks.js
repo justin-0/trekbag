64a7ee05d01a37bb0159a47616cda12d90524d48
Justin OConnell <itsjustinoconnell@gmail.com>
2024-01-18 11:47:13 +0000

    added custom error in case context is used outside provider

--- src/lib/hooks.js
+++ src/lib/hooks.js
@@ -2,5 +2,13 @@ import { useContext } from "react";
 import { ItemContext } from "../contexts/ItemContextProvider";
 
 export const useItemsContext = () => {
-  return useContext(ItemContext);
+  const context = useContext(ItemContext);
+
+  if (!context) {
+    throw new Error(
+      "useItemsContext is not available, must be used within ItemContextProvider"
+    );
+  }
+
+  return context;
 };
