d253826256bfbd902bf78b0bcc78d672216f5d44
Justin OConnell <itsjustinoconnell@gmail.com>
2024-01-18 10:46:04 +0000

    persisted data with local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { initialItems } from "./constants/initial-items";
 import BackgroundHeading from "./components/BackgroundHeading";
 import Footer from "./components/Footer";
@@ -7,12 +7,12 @@ import ItemList from "./components/ItemList";
 import Sidebar from "./components/Sidebar";
 
 export default function App() {
-  const [items, setItems] = useState(initialItems);
+  const localStorageItems = JSON.parse(localStorage.getItem("items"));
+
+  const [items, setItems] = useState(localStorageItems || initialItems);
 
   const totalItems = items.length;
   const totalPacked = items.filter((item) => item.packed === true).length;
-  console.log(totalPacked);
-
   const handleAddItem = (newItem) => {
     const newItems = [...items, newItem];
     setItems(newItems);
@@ -51,6 +51,10 @@ export default function App() {
 
   const handleDeleteItem = (id) =>
     setItems(items.filter((item) => item.id !== id));
+
+  useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(items));
+  }, [items]);
 
   return (
     <>
