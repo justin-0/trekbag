62909be5e1f79f00c5a8629c241c948d2cb21753
Justin OConnell <itsjustinoconnell@gmail.com>
2024-01-18 09:46:30 +0000

    feat: installed react-select package

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,13 +1,32 @@
 import EmptyView from "./EmptyView";
+import { useState } from "react";
+import Select from "react-select";
+
+const options = [
+  { value: "chocolate", label: "Chocolate" },
+  { value: "strawberry", label: "Strawberry" },
+  { value: "vanilla", label: "Vanilla" },
+];
 
 export default function ItemList({
   items,
   handleCompleteItem,
   handleDeleteItem,
 }) {
+  const [selectedOption, setSelectedOption] = useState(null);
+
   return (
     <ul className="item-list">
       {items.length === 0 ? <EmptyView /> : null}
+      {items.length > 0 ? (
+        <div className="sorting">
+          <Select
+            defaultValue={selectedOption}
+            onChange={setSelectedOption}
+            options={options}
+          />
+        </div>
+      ) : null}
       {items.map((item) => (
         <Item
           key={item.id}
